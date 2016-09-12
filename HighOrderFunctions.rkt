141caf045592f2132b5dbf7cbef634f56b148232
SolarFlar3 <jacobanderson1104@gmail.com>
2016-09-12 08:23:42 -0600

    Almost finished with Higher Order Functions

---
+++ HighOrderFunctions.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(define (convertCelc temp)
+  (/ (- temp 32) (/ 9 5)))
+
+(define (convertFC temps)
+  (map convertCelc temps))
+
+(define (check-temp1 temp)
+  (cond
+    ((> temp 95) false)
+    ((< temp 4) false)
+    (else true)))
+
+(define (check-temps1 temps)
+  (andmap check-temp1 temps))
+
+(define (check-temps temps low high)
+  (andmap (lambda (x)
+            (cond
+              ((> x high) false)
+              ((< x low) false)
+              (else true))) temps))
+
+(define (next-element-bigger digits mult)
+  (if (empty? digits)
+      (list 0)
+      (append (list (* (first digits) mult)) (next-element-bigger (rest digits) (* mult 10)))))
+      
+(define (convert digits)
+  (foldr + 0 (next-element-bigger digits 1)))
+
+(define (double item)
+  (cons item (list item)))
+
+(define (duple lst)
+  (map double lst))
+
+(define (average lst)
+  (/ (foldr + 0.0 lst) (length lst)))
+
+(define (eliminate-larger lst)
+  (filter  lst))
+
+(define (curry2 func) ...)
